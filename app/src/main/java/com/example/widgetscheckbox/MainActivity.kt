f82daa096789980e13a1c842d9d5b1a72a69e53b
jejxis <3190021@gmail.com>
2022-04-13 18:06:53 +0900

    WidgetsCheckBox

---
+++ app/src/main/java/com/example/widgetscheckbox/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.widgetscheckbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.CompoundButton
+import com.example.widgetscheckbox.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    val listener by lazy{
+        CompoundButton.OnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked){
+                when(compoundButton.id){
+                    R.id.checkApple -> Log.d("CheckBox", "사과가 선택되었습니다.")
+                    R.id.checkBanana -> Log.d("CheckBox", "바나나가 선택되었습니다.")
+                    R.id.checkOrange -> Log.d("CheckBox", "오렌지가 선택되었습니다.")
+                }
+            }else{
+                when(compoundButton.id){
+                    R.id.checkApple -> Log.d("CheckBox", "사과가 선택 해제되었습니다.")
+                    R.id.checkBanana -> Log.d("CheckBox", "바나나가 선택 해제되었습니다.")
+                    R.id.checkOrange -> Log.d("CheckBox", "오렌지가 선택 해제되었습니다.")
+                }
+            }
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        /*binding.checkApple.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked) Log.d("CheckBox", "사과가 선택되었습니다.")
+            else Log.d("CheckBox", "사과가 선택 해제되었습니다.")
+        }*/
+
+        binding.checkApple.setOnCheckedChangeListener(listener)
+        binding.checkBanana.setOnCheckedChangeListener(listener)
+        binding.checkOrange.setOnCheckedChangeListener(listener)
+    }
+}
